b90a7a56a573de262a6df923e1d9a67ee82f5937
krec-kiran <krec.kiran@gmail.com>
2019-03-17 16:07:52 +0000

    REST endpoints creation

--- app.py
+++ app.py
@@ -7,7 +7,40 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-# Kiran git test
+
+# POST - to receive data
+# GET - to send data
+
+# POST /store/data:{name}
+@app.route('/store', methods=['POST'])
+def create_store():
+    pass
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    # print('Sainsburys!')
+    pass
+
+
+# GET /store
+@app.route('/store')
+def get_stores():
+    pass
+
+
+# POST /store/<string:name>/item:{name:,price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store():
+    pass
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    pass
+
 
 if __name__ == '__main__':
     app.run()
